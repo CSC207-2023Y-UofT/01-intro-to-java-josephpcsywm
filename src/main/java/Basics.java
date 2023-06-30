3acf30f7a141718f4b2ecc51e374f2c02852382a
josephpcsywm <128923724+josephpcsywm@users.noreply.github.com>
2023-06-29 21:50:23 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,11 +43,12 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
         /* 4. In Python, we could declare variables using a simple assignment
-         *    statement. For example:
+         *    statement  For example:
          *        x = 10
          *    In Java, we have to specify the variable's type as well:
          *        int x = 10;
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +100,20 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
+
+        /*
+         *
+         * String to_split = "Idol Long Oolong Vertical Europe University Toyota";
+         * System.out.println(split(to_split));
+         *
+         *
+         * int[] array = {1, 2};
+         * System.out.println(oddSum(array));
+         */
+
 
 
     }
@@ -143,6 +158,11 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] words = to_split.split(" ", 7);
+
+        for (int i = 0; i <= 6; i++){
+            ret.append(words[i].charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +190,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        for (int i = 1; i < arr.length; i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
